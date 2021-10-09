05d802abd4eb15680c412b06888791a1f44cf649
Denis <frontendsunfish@gmail.com>
2021-10-09 17:30:37 +0300

    Auth comlete

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,4 +1,4 @@
-import {useState, useCallBack, useEffect} from 'react';
+import {useState, useCallback, useEffect} from 'react';
 
 const storageName = 'userData';
 
@@ -6,16 +6,16 @@ export const useAuth = () => {
     const [token, setToken] = useState (null);
     const [userId, setUserId] = useState (null);
 
-    const login = useCallBack((jwtToken, id)=> {
+    const login = useCallback((jwtToken, id) => {
         setToken(jwtToken);
         setUserId(id);
 
         localStorage.setItem(storageName, JSON.stringify({
-            userId, token
+            userId: id, token: jwtToken
         }));
     }, []);
 
-    const logout = useCallBack(()=> {
+    const logout = useCallback(()=> {
         setToken(null);
         setUserId(null);
         localStorage.removeItem(storageName);
