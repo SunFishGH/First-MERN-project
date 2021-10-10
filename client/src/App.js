b28e4e5b70ee5fff46ffb802038172ee99dc5955
Denis <frontendsunfish@gmail.com>
2021-10-10 22:01:56 +0300

    DetailPage complete

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,16 @@ import { useAuth } from './hooks/auth.hook';
 import 'materialize-css';
 import { AuthContext } from './context/AuthContext';
 import {NavBar} from './components/NavBar';
+import { Loader } from './components/Loader';
 
 function App() {
-  const {token, login, logout, userId} = useAuth();
+  const {token, login, logout, userId, ready} = useAuth();
   const isAutheticated = !!token;
   const routes = useRoutes(isAutheticated);
+  
+  if(!ready){
+    return <Loader />
+  }
   return (
     <AuthContext.Provider value={{
       token, login, logout, userId, isAutheticated
--- routes/link.routes.js
+++ routes/link.routes.js
@@ -47,7 +47,7 @@ router.get('/', auth, async (req, res) => {
 router.get('/:id', auth, async (req, res) => {
     
     try{
-        const links = await Link.findById(req.params.id); 
+        const link = await Link.findById(req.params.id); 
         res.json(link);
     } catch (e) {
         res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'});
